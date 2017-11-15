7246f79862eda733626b7505884319a0399b4cfa
Alexandre Sieira <alexandre.sieira@gmail.com>
2017-11-15 18:32:36 -0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -33,6 +33,10 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 5 - Production/Stable',
         'Natural Language :: English',
         'Environment :: Other Environment',
